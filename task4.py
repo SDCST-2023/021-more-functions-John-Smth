ef8a7416290325b792603af03c7a57605bdd4c42
Jack <124203482+John-Smth@users.noreply.github.com>
2023-04-11 11:22:26 -0700

    1

--- task4.py
+++ task4.py
@@ -11,8 +11,12 @@ note: Area of a circle is given by A = pi*(square of the radius)
 You may want to use the math module to complete this problem
 """
 
-def area():
-    return
+def area(a):
+    AM1 = a **2
+    AM2 = AM1 * 3.14159265359
+    Area = round(AM2, 2)
+    
+    return Area
 
 
 assert round(area(2),2) == 12.57
